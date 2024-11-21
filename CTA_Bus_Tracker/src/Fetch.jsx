7e7562ceb479ffc70479af5877557db17a9a6af2
sramirez124 <forwardemails124@gmail.com>
2024-11-21 00:51:53 -0600

    starting the fetch process for api data

--- CTA_Bus_Tracker/src/Fetch.jsx
+++ CTA_Bus_Tracker/src/Fetch.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "dotenv/config"
 
 function Fetch() {
-
+    const API_Key = process.env.API_Key
+    const Fetch = () => {
+        const [data, setData] = useState([])
+        useEffect(() => {
+            fetch(`http://www.ctabustracker.com/bustime/api/v2/gettime?key=${API_Key}`)
+        })
+    }
 }
 
 export default Fetch
